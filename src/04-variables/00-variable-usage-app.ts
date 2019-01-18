54aa5880f3f167866a41a39696a23294d92dfc35
Jeffrey-Young <youngjt@live.unc.edu>
2019-01-18 14:47:05 -0500

    lec4

---
+++ src/04-variables/00-variable-usage-app.ts
@@ -0,0 +1,17 @@
+import { print } from "introcs";
+
+export let main = async () => {
+
+    // Variable Declaration
+
+    // Variable Assignment and Initialization
+
+    // Variable Access
+
+    // Implicit Variable Declaration and Initialization
+
+    // Number Variable Increment
+    
+};
+
+main();
---
+++ src/04-variables/01-prompting-app.ts
@@ -0,0 +1,11 @@
+import { print, promptString } from "introcs";
+
+export let main = async () => {
+
+    let name = await promptString("What is your name?");
+
+    // TODO: Using concatenation, print an uplifting message to the user.
+
+};
+
+main();
