3b1027c8070dfd5564532c0cfc84abd736704a06
Jeffrey-Young <youngjt@live.unc.edu>
2019-02-01 13:16:43 -0500

    lec08

---
+++ src/08-logical-operators/00-happy-and-you-know-it-app.ts
@@ -0,0 +1,12 @@
+import { print, promptBoolean } from "introcs";
+
+export let main = async () => {
+
+    let happy: boolean = await promptBoolean("Are you happy?");
+    // TODO: Declare a boolean "knowIt" and prompt the user
+    
+    // TODO: if happy and knowIt are both true, print "Clap your hands"
+    
+};
+
+main();
