536f7522adb3fa58012146ba31849d0a25cd16d8
Jeffrey-Young <youngjt@live.unc.edu>
2019-02-15 10:28:18 -0500

    lec13

---
+++ src/13-arrays/01-loop-through-array-app.ts
@@ -0,0 +1,15 @@
+import { print } from "introcs";
+
+export let main = async () => {
+
+    let a: number[] = [2, 3, 5, 7, 11, 13];
+
+    let i: number = 0;
+
+    // TODO: Write a while loop here
+    
+    // Don't forget to increment i by 1 inside the loop!
+
+};
+
+main();
---
+++ src/13-arrays/02-sum-array-app.ts
@@ -0,0 +1,21 @@
+import { print } from "introcs";
+
+export let main = async () => {
+
+    let a: number[] = [2, 3, 5, 7, 11, 13];
+    let result: number = 0;
+
+    let i: number = 0;
+
+    // TODO: Write a while loop here
+    // It should:
+    // 1. Loop while i is less than a's length
+    // 2. Inside the loop:
+    // 2.1 Increase the result variable by a[i]'s value
+    // 2.2 Increment i by 1
+
+    // TODO: After the loop completes, print result
+
+};
+
+main();
