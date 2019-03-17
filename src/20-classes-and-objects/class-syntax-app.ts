b69ac0366a5131f9a44db7f5e6d02b4e88ae8b72
Jeffrey-Young <youngjt@live.unc.edu>
2019-03-17 12:03:04 -0400

    lec20

---
+++ src/20-classes-and-objects/class-syntax-app.ts
@@ -0,0 +1,11 @@
+import { print } from "introcs";
+
+export let main = async () => {
+
+    // TODO #1: Construct a TwitterProfile object
+
+};
+
+// TODO #0: Define a TwitterProfile Class
+
+main();
