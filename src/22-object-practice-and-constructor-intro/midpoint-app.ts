d448b0cf7191f72e88ca65516667057ff3755d37
Jeffrey-Young <youngjt@live.unc.edu>
2019-03-20 14:14:10 -0400

    lec22

---
+++ src/22-object-practice-and-constructor-intro/midpoint-app.ts
@@ -0,0 +1,32 @@
+import { print } from "introcs";
+
+class Point {
+    x: number = 0;
+    y: number = 0;
+
+    // TODO #5: Define a constructor that initializes the x and y properties of a Point
+
+}
+
+export let main = async () => {
+
+    let a = new Point();
+    // TODO #1: Assign different values to a's x and y properties
+
+    // TODO #2: Declare a Point variable named b and initialize it
+
+    // TODO #3: call the midpoint function with 
+    // points a and b and print the result
+
+};
+
+// TODO #4: Implement midpoint
+let midpoint = (p1: Point, p2: Point): Point => {
+    // 1. Declare a variable named mid and assign it a new Point object
+    // 2. Assign its x and y properties based on the midpoint formula.
+    // 3. Return mid instead of p1.
+    return p1;
+};
+
+
+main();
