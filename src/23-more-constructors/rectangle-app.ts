d1019ec05d998951866106fc571fc53ff5c78f82
Jeffrey-Young <youngjt@live.unc.edu>
2019-03-22 12:49:17 -0400

    lec23

---
+++ src/23-more-constructors/rectangle-app.ts
@@ -0,0 +1,36 @@
+import { print } from "introcs";
+
+class Rectangle {
+    width: number = 0;
+    height: number = 0;
+    isSquare: boolean;
+
+    // TODO #1: Write a constructor that initializes all 3 properties
+
+}
+
+export let main = async () => {
+
+    // TODO #2: Uncomment
+    // let rects: Rectangle[] = [];
+    // rects[0] = new Rectangle(1, 1);
+    // rects[1] = new Rectangle(3, 1);
+    // rects[2] = new Rectangle(5, 5);
+    // rects[3] = new Rectangle(1, 2);
+    // print(rects);
+    
+    // TODO #4: Call the countSquares function and pass rects as an argument
+
+};
+
+let countSquares = (rects: Rectangle[]): number => {
+    // TODO #3: Implement the countSquares function
+    // 0. Declare and initialize a variable to track the count
+    //    of squares in the array
+    // 1. Loop through each element in the array
+    // 2. if the element is a square, increment the counting variable
+    // 3. return the count of squares
+    return -1; // change this
+};
+
+main();
