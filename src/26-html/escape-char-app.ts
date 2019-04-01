27963d6b0d9ad4ec927f430c762a3d93c45828bf
Jeffrey-Young <youngjt@live.unc.edu>
2019-04-01 14:28:48 -0400

    lec26

---
+++ src/26-html/escape-char-app.ts
@@ -0,0 +1,8 @@
+import { print } from "introcs";
+
+export let main = async () => {
+    print("This string will be printed \n on two lines");
+    print("Martin Luther King Jr. said: \"Injustice anywhere is a threat to justice everywhere\"");
+    print("This is how you print a \\");
+};
+main();
---
+++ src/26-html/game-script.ts
@@ -0,0 +1,20 @@
+import { random } from "introcs";
+
+// Initialize variables connected to HTML objects
+let input = document.getElementById("input") as HTMLInputElement;
+let submit = document.getElementById("submit") as HTMLButtonElement;
+// TODO #1: Initialize the response variable
+
+// Generate a random number
+let answer = random(1, 100);
+
+export let main = async () => {
+    // TODO #0: Assign a color to the background to be magenta
+
+    // TODO #3: Assign submit's onclick property to checkGuess
+
+};
+
+// TODO #2: Establish a checkGuess function
+
+main();
