a0e771fcaa0852ecc5f855ccca2e70b886ae342f
Jeffrey-Young <youngjt@live.unc.edu>
2019-04-15 14:55:23 -0400

    lec31

---
+++ src/31-2d-arrays/2d-arrays-app.ts
@@ -0,0 +1,37 @@
+import { print } from "introcs";
+
+export let main = async () => {
+    // Declare a 2d number array
+
+    // Initialize using Literals
+
+    // Initialize using array2d helper function
+
+    // Assigning to an element
+
+    // Read the # of rows
+
+    // Read the # of cols
+
+    // Read from an element
+
+    // Read from a top-level element
+
+    // Inspect complete array in developers' tools console
+    
+};
+
+/**
+ * Given the number of rows and columns, and an initial value,
+ * this function will initialize a new 2d array of that size and
+ * assign the initial value to each element.
+ */
+let array2d = (rows: number, cols: number, value: number): number[][] => {
+    let a: number[][] = [];
+
+    // TODO
+
+    return a;
+};
+
+main();
