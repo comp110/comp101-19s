a515469d834903087764b42b4f332906f08d2af1
Jeffrey-Young <youngjt@live.unc.edu>
2019-04-15 23:09:10 -0400

    ps08

---
+++ src/ps08-funmoji/funmoji.ts
@@ -0,0 +1,34 @@
+/**    
+ * Author:
+ * ONYEN:
+ * UNC Honor Pledge: I certify that no unauthorized assistance has been received   
+ * or given in the completion of this work. I certify that I understand and 
+ * could now rewrite on my own, without assistance from course staff,  
+ * the problem set code I am submitting.
+ */
+
+// A number of Shapes are imported for you.
+// To learn more about how to use them, use the
+// documentation website: 
+// https://comp110.com/introcs-graphics/index.html
+import {
+    Group,
+    Rectangle,
+    Circle,
+    Ellipse,
+    Line,
+    Stroke,
+    Color
+} from "introcs/graphics";
+
+export let funmoji = (): Group => {
+    let shapes = new Group();
+
+    // TODO: Call helper functions to generate groups of shapes
+    // for the specific components of your funmoji. You will need
+    // to add their return values to the shapes Group variable.
+    
+    return shapes;
+};
+
+// TODO: Define your 3 helper functions below.
