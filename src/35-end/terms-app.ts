7c690cb4c8930c37bade8119d2b44fe63db5095d
Jeffrey-Young <youngjt@live.unc.edu>
2019-04-26 11:56:34 -0400

    end

---
+++ src/35-end/terms-app.ts
@@ -0,0 +1,29 @@
+import { print, promptNumber, random } from "introcs";
+import { dictionary } from "./terms";
+
+export let main = async () => {
+    // TODO #1.2 - Call randomTerm and print its result.
+
+    // TODO #2.2 - Uncomment the lines below.
+    // let termCount = await promptNumber("How many terms?");
+    // print(randomTerms(termCount));
+
+    // TODO #3.2 - Test your contains function
+};
+
+// TODO #1: Define randomTerm Function
+
+let randomTerms = (n: number): string[] => {
+    let terms: string[] = [];
+    // TODO #2.2: Append n random terms to the terms array
+
+    return terms;
+};
+
+// TODO #3: Implement contains
+let contains = (haystack: string[], needle: string): boolean => {
+    // TODO #3.1
+    return false;
+};
+
+main();
